cb00c9d1100041da8a67e18f4a0789776013a279
Chad Fowler <chad@chad-fowlers-mac-pro.local>
2008-10-22 08:36:07 -0600

    Add generated session configuration. (This commit was generated by blank's new_app thor task.)

---
+++ config/session.rb
@@ -0,0 +1,8 @@
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+SESSION_CONFIG = {
+  :session_key => '_plzaskus_session',
+  :secret      => '5a2d4fe53bb2ebf2dab3b810ae800c2b3e1ef26d9ff451efe4bc2a1d57b1da5272763f2242bb6c4aa49eb18fd2814e7ddf18fbb998a93078076f44a7c35cb50b'
+}.freeze
